3fbeb156703d6141a902113744953c892a3bf644
Akorede Fodilu Olawale <afordeal88@gmail.com>
2020-03-15 19:52:56 +0000

    last

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -9,7 +9,7 @@ import { Scrumuser } from '../scrumuser';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-  WS_URL: string ="wss://gr3cwo64fb.execute-api.us-west-2.amazonaws.com/Test/" //"wss://fh7hhc2pn0.execute-api.us-east-2.amazonaws.com/Test" 
+  WS_URL: string ="wss://gr3cwo64fb.execute-api.us-west-2.amazonaws.com/Test" //"wss://fh7hhc2pn0.execute-api.us-east-2.amazonaws.com/Test" 
   websocketConnection: WebSocket ;
   messages: Array<object> =[]
   chat_text: String
@@ -20,21 +20,41 @@ export class ChatComponent implements OnInit {
   socket: WebSocket;
 
   constructor( private wsService: WebsocketService) {
-    websocketConnection: WebSocket
-    this.websocketConnection = new WebSocket(this.WS_URL)
+    this.websocketConnection = new WebSocket('wss://gr3cwo64fb.execute-api.us-west-2.amazonaws.com/Test');
+    console.log('connect', this.websocketConnection)
   }
 
   ngOnInit() {
-    this.websocketConnection.onopen = (event)=>{
-      const context = {action:"getRecentMessages"}
+    this.websocketConnection.onopen = (event) => { 
+      const context = { action: "getRecentMessages" }
       this.websocketConnection.send(JSON.stringify(context))
-    }
-    this.websocketConnection.onmessage = (event)=>{
-      let data = JSON.parse(event.data)
-      if(data['messages']!==undefined){
-        data['messages'].forEach((message) => { this.messages.push(message)});
+
+      this.websocketConnection.onmessage = (event) =>{
+        let data = JSON.parse(event.data)
+        console.log("message things" + data['messages'])
+        if (data['messages'] !== undefined){
+          data['messages'].forEach((message) => {
+            console.log("new =" + JSON.stringify(message))
+            this.messages.push(message)
+            
+          })
+          console.log("Array "+ this.messages)
+          this.messages.forEach((message) =>{
+            console.log("allmessages " + message )
+          })
+        }
       }
     }
+    // this.websocketConnection.onopen = (event)=>{
+    //   const context = {action:"getRecentMessages"}
+    //   this.websocketConnection.send(JSON.stringify(context))
+    // }
+    // this.websocketConnection.onmessage = (event)=>{
+    //   let data = JSON.parse(event.data)
+    //   if(data['messages']!==undefined){
+    //     data['messages'].forEach((message) => { this.messages.push(message)});
+    //   }
+    // }
   }
   scrumLoginUserModel = new Scrumuser('', '', '', '', '');
 
